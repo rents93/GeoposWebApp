8e600ee6afac09b39c36df200d57fb1c23e746de
Gianluca P <giangi.inter@hotmail.it>
2018-04-11 17:27:21 +0200

    Prova

--- src/com/positionWebApp/Position.java
+++ src/com/positionWebApp/Position.java
@@ -10,7 +10,7 @@ public class Position {
         this.longitude = lon;
         this.timestamp = ts;
         /*ciao*/
-        /*commit2*/
+        /*ciao*/
     }
 
     public double getLatitude() {
